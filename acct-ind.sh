871b0d837ed1274a012f3fc3b3ed5ac5e0554e9a
Dan Bednarski <marioknight@missdream.org>
2015-10-19 08:47:55 -0400

    Determine account document root from user file instead of default path

--- acct-ind.sh
+++ acct-ind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-path="/home/$i/public_html"
 domain=`cat /var/cpanel/userdata/$i/main | grep "main_domain" | awk '{ print $2}'`
+path=`cat /var/cpanel/userdata/$i/$domain | grep "documentroot" | awk '{ print $2}'`
